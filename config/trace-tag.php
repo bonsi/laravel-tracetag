49477e95d3d0de01d98506088e4dd94f101902ea
Ivo Bons <ivo@virtuosomedia.nl>
2017-06-30 15:10:35 +0200

    add a configuration file

--- config/trace-tag.php
+++ config/trace-tag.php
@@ -6,18 +6,28 @@ return [
     |--------------------------------------------------------------------------
     | Laravel TraceTag
     |--------------------------------------------------------------------------
-    |
-    | Here are the valid tag generators to choose from.
+    */
+    //    'enabled' => true,
+
+    /*
     |
     | The generator will default to Bonsi\TraceTag\Generators\RandomInt::class.
     |
     */
-
     'generator' => Bonsi\TraceTag\Generators\RandomInt::class,
 //    'generator' => Bonsi\TraceTag\Generators\Uuid4::class,
 
+
     'middleware' => [
-        'enabled' => true,
+        'enabled' => false,
+        /*
+        |
+        | If headerName is set on the request, set the TraceTag to this value.
+        | The same value will be added to the response as a header as well.
+        | inputName can also be used to set the TraceTag value but the
+        | value from the header will always take precedence.
+        |
+        */
         'headerName' => 'X-Trace-Tag',
         'inputName' => '_tracetag',
     ]
