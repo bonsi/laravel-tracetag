89a9b34004e7a7417ae656887fd29735154ca9fc
Ivo Bons <ivo@virtuosomedia.nl>
2017-06-30 15:15:15 +0200

    update for configurable values, automatically add the middleware, automatically register the facade

--- src/TraceTagServiceProvider.php
+++ src/TraceTagServiceProvider.php
@@ -1,14 +1,18 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Bonsi\TraceTag;
 
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Log;
 use Bonsi\TraceTag\Generators\Uuid4;
-use Bonsi\TraceTag\Generators\RandomInt;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Application;
 use \Illuminate\Support\ServiceProvider;
+use Bonsi\TraceTag\Generators\RandomInt;
+use Bonsi\TraceTag\Middleware\TraceTagMiddleware;
 
 class TraceTagServiceProvider extends ServiceProvider
 {
-//    protected $defer = true;
 
     /**
      * Bootstrap the application services.
@@ -17,7 +21,18 @@ class TraceTagServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        if(!$this->app->config->get('trace-tag.enabled', false)) {
+//            return;
+//        }
+
+        $source = realpath(__DIR__ . '/../config/trace-tag.php');
+        $this->publishes([$source => config_path('trace-tag.php')]);
+        $this->mergeConfigFrom($source, 'trace-tag');
+
+        if($this->app->config->get('trace-tag.middleware.enabled', false))
+        {
+            $this->registerMiddleware(TraceTagMiddleware::class);
+        }
     }
 
     /**
@@ -27,9 +42,31 @@ class TraceTagServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(TraceTag::class, function($app) {
-            return new TraceTag(new RandomInt);
-//            return new TraceTag(new Uuid4);
+//        if(!$this->app->config->get('trace-tag.enabled', false)) {
+//            return;
+//        }
+
+        Log::getMonolog()->pushProcessor(new \Bonsi\TraceTag\Integrations\MonologProcessor);
+
+        $this->app->singleton('tracetag', function(Container $app) {
+            $generatorClass = $app->config->get('tracetag.generator', RandomInt::class);
+            return new TraceTag(new $generatorClass);
         });
+
+        $this->app->alias('tracetag', TraceTag::class);
     }
+
+    /**
+     * Register the TraceTag middleware.
+     *
+     * @param $middleware
+     */
+    public function registerMiddleware( $middleware )
+    {
+        $kernel = $this->app[Kernel::class];
+
+//        $kernel->prependMiddleware($middleware);
+        $kernel->pushMiddleware($middleware);
+    }
+
 }
